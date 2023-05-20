931148e1131637e2b6300619b89560033a33be46
zahrahashimli <zahrarh@code.edu.az>
2023-05-20 08:05:34 +0400

    text added

---
+++ hwdue20th/hwdue20th/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace hwdue20th
+{
+    class DebitCard
+    {
+        public string OwnerName { get; set; }
+        public string OwnerSurname { get; set; }
+        public string CompanyName { get; set; }
+        public string CardNumber { get; set; }
+        public decimal Balance { get; set; }
+        public string CVVCode { get; set; }
+
+        public void IncomeBalance(decimal money)
+        {
+            if (money <= 0) return;
+
+            Balance += money;
+
+        }
+
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                DebitCard myDebitCard = new DebitCard();
+
+                myDebitCard.OwnerName = "Zahra";
+                myDebitCard.OwnerSurname = "Hashimli";
+                myDebitCard.CompanyName = "Unibank";
+                myDebitCard.CardNumber = "1234567812345678";
+                myDebitCard.CVVCode = "111";
+
+                myDebitCard.IncomeBalance(50);
+
+                Console.WriteLine(myDebitCard);
+
+
+
+            }
+
+
+
+
+
+        }
+
+
+
+
+
+    }
+
+
+
+
+}
